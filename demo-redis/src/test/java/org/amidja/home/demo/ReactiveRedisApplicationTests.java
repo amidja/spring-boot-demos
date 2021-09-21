578d3dee34b4bc347b9a956d8f8c24f1b54812cb
Amidja <jasminsabina@yahoo.com>
2021-09-21 15:10:31 +1000

    demo-redis init import

---
+++ demo-redis/src/test/java/org/amidja/home/demo/ReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amidja.home.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
