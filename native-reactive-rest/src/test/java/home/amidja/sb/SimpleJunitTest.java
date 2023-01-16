ca757d26317582317d1fe9d4c998f735a400fc75
Amidja <jasminsabina@yahoo.com>
2023-01-16 14:16:19 +1100

    switch to 3.0.1 version of spring boot framework

---
+++ native-reactive-rest/src/test/java/home/amidja/sb/SimpleJunitTest.java
@@ -0,0 +1,14 @@
+package home.amidja.sb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleJunitTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
